d5247c36c4c054ffcf464669f60854e695940100
Jerrell McCormick <jerrellmccormick2@gmail.com>
2019-10-20 22:20:11 +0000

    10.39

--- app/models/place.rb
+++ app/models/place.rb
@@ -5,5 +5,7 @@ class Place < ApplicationRecord
   geocoded_by :address
   after_validation :geocode
   
-  validates :name, presence: true
+  validates :address, :description, presence: true
+  validates :name, length: { minimum: 3 }
+
 end
