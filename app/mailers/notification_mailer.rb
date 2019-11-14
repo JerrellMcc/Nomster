9fe6e2a7bdfbed854f849351b01b570b280cf3fc
Jerrell McCormick <jerrellmccormick2@gmail.com>
2019-11-14 17:08:52 +0000

    11.41

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
     default from: "no-reply@nomsterapp.com"
 
   def comment_added
-    mail(to: "jerrellmccormick2@gmail.com",
+    mail(to: "calmmccormick@gmail.com",
           subject: "A comment has been added to your place")
   end
 
